b9d5228e742ad5900c1fa0d8070623eded33c69a
Jeppe Boelsmand <jeppe@kyomu.dk>
2013-03-12 19:14:03 +0100

    Updates

--- magicTrader/urls.py
+++ magicTrader/urls.py
@@ -10,7 +10,7 @@ from cards import views
 
 urlpatterns = patterns('',
     url(r'^dispatch/$', 'traders.views.traderDispatcher', name='traderDispatcher'),
-    url(r'^gathercard/(.+)[/$]', 'cards.views.gatherCard', name='gather card'),
+    url(r'^gathercard/(.+?)[/$]', 'cards.views.gatherCard', name='gather card'),
     url(r'^card/(.+)[/$]', 'cards.views.getCard', name='get card'),
     url(r'^cards/(.*?)[/$]', 'cards.views.cardList', name='list cards'),
     url(r'^fiximages[/$]', 'cards.views.fixImages', name='fix cards and sets'),
